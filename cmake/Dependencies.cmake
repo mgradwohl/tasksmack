bbe8331c15e6cae7856cb33c5eb41b0bb3055b2b
Matt Gradwohl <mattgr@msn.com>
2026-08-25 21:15:58 -0700

    Fix SDL CMP0219 policy default

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -103,13 +103,10 @@ set(SDL_X11_XTEST OFF CACHE BOOL "" FORCE)  # We don't need XTEST
 set(SDL_X11_XDBE OFF CACHE BOOL "" FORCE)  # We don't need double buffering extension
 
 if(POLICY CMP0219)
-    cmake_policy(PUSH)
-    cmake_policy(SET CMP0219 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0219 NEW)
 endif()
 FetchContent_MakeAvailable(SDL3)
-if(POLICY CMP0219)
-    cmake_policy(POP)
-endif()
+unset(CMAKE_POLICY_DEFAULT_CMP0219)
 set_target_properties(SDL3-static PROPERTIES FOLDER "third_party")
 
 # Prefer the project-local environment created by tools/setup-dev.sh without
