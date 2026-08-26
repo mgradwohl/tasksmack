3d0fbe6a63ef240a9fc81c69085239413b8fc06b
Matt Gradwohl <mattgr@msn.com>
2026-08-26 07:29:08 -0700

    Fix SDL CMP0219 policy default (#692)
    * Fix SDL CMP0219 policy default
    
    * Preserve CMP0219 policy default

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -102,14 +102,22 @@ set(SDL_X11_XSCRNSAVER OFF CACHE BOOL "" FORCE)  # We don't need screensaver sup
 set(SDL_X11_XTEST OFF CACHE BOOL "" FORCE)  # We don't need XTEST
 set(SDL_X11_XDBE OFF CACHE BOOL "" FORCE)  # We don't need double buffering extension
 
+set(_TASKSMACK_RESTORE_CMP0219_DEFAULT FALSE)
+if(DEFINED CMAKE_POLICY_DEFAULT_CMP0219)
+    set(_TASKSMACK_SAVED_CMP0219_DEFAULT "${CMAKE_POLICY_DEFAULT_CMP0219}")
+    set(_TASKSMACK_RESTORE_CMP0219_DEFAULT TRUE)
+endif()
 if(POLICY CMP0219)
-    cmake_policy(PUSH)
-    cmake_policy(SET CMP0219 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0219 NEW)
 endif()
 FetchContent_MakeAvailable(SDL3)
-if(POLICY CMP0219)
-    cmake_policy(POP)
+if(_TASKSMACK_RESTORE_CMP0219_DEFAULT)
+    set(CMAKE_POLICY_DEFAULT_CMP0219 "${_TASKSMACK_SAVED_CMP0219_DEFAULT}")
+else()
+    unset(CMAKE_POLICY_DEFAULT_CMP0219)
 endif()
+unset(_TASKSMACK_RESTORE_CMP0219_DEFAULT)
+unset(_TASKSMACK_SAVED_CMP0219_DEFAULT)
 set_target_properties(SDL3-static PROPERTIES FOLDER "third_party")
 
 # Prefer the project-local environment created by tools/setup-dev.sh without
